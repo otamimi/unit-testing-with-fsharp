ae87fc324be6390237522b4b5c21642376f4f78b
osama <osama.tamimi@outlook.com>
2019-10-05 19:47:55 +0300

    add two numbers with comma refactor to testcases

--- CalculatorService/Library.fs
+++ CalculatorService/Library.fs
@@ -6,4 +6,7 @@ module Calculate =
     let Add inputString =
        match inputString with
        | "" -> 0
+       | _ when inputString.Contains "," ->
+            let numbers = inputString.Split [| ',' |]
+            (Int32.Parse numbers.[0]) + (Int32.Parse numbers.[1])
        | _ -> Int32.Parse inputString
--- CalculatorServiceTests/UnitTest1.fs
+++ CalculatorServiceTests/UnitTest1.fs
@@ -8,16 +8,13 @@ open CalculatorService
 [<TestFixture>]
 module TestClass  =
 
-    [<Test>]
-    let EmptyStringReturnsZero() =
-        let expected = 0
-        let actual = Calculate.Add ""
-        Assert.That(actual, Is.EqualTo(expected))
-
+    [<TestCase("", ExpectedResult  = 0)>]
     [<TestCase("1", ExpectedResult  = 1)>]
     [<TestCase("100", ExpectedResult  = 100)>]
-    let SingleNumberReturnsSelf inputString =
-         Calculate.Add inputString
+    [<TestCase("1,2", ExpectedResult  = 3)>]
+    [<TestCase("2,3", ExpectedResult  = 5)>]
+       let AddTwoNumbers_CommaDelimiter_Returns_Sum inputString =
+            Calculate.Add inputString
 
 
    
