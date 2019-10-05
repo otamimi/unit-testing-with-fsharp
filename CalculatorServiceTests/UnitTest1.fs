cd6933eedc78ad045be5ca00719e92e11b2f4d3e
osama <osama.tamimi@outlook.com>
2019-10-05 19:36:59 +0300

    refactor and improve add TestCases

--- CalculatorServiceTests/UnitTest1.fs
+++ CalculatorServiceTests/UnitTest1.fs
@@ -6,17 +6,20 @@ open CalculatorService
 
 
 [<TestFixture>]
-type TestClass () =
+module TestClass  =
 
     [<Test>]
-    member this.EmptyStringReturnsZero() =
+    let EmptyStringReturnsZero() =
         let expected = 0
         let actual = Calculate.Add ""
         Assert.That(actual, Is.EqualTo(expected))
 
-   // [<TestCase("1", Result = 1)>]
-    [<Test>]
-    member this.SingleNumberReturnsSelf() =
-        let expected = 6
-        let actual = Calculate.Add expected
-        Assert.That(actual, Is.EqualTo(expected))
+    [<TestCase("1", ExpectedResult  = 1)>]
+    [<TestCase("100", ExpectedResult  = 100)>]
+    let SingleNumberReturnsSelf inputString =
+         Calculate.Add inputString
+
+
+   
+        
+
