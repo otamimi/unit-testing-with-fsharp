8e1ffcfd1ab68f14f6bb9fc785e2f3d76874fc7d
osama <osama.tamimi@outlook.com>
2019-10-05 20:50:18 +0300

    remove unnecessary match cases

--- CalculatorService/Library.fs
+++ CalculatorService/Library.fs
@@ -3,10 +3,12 @@
 open System
 
 module Calculate =
+
+   
     let Add inputString =
-       match inputString with
-       | "" -> 0
-       | _ when inputString.Contains "," ->
-            [ for number in inputString.Split[| ','; '\n'|] ->
-                Int32.Parse number] |> List.reduce (fun n1 n2 -> n1 + n2)
-       | _ -> Int32.Parse inputString
+          match inputString with
+          | "" -> 0
+          | _  ->
+               [ for number in inputString.Split[| ','; '\n'|] ->
+                   Int32.Parse number] |> List.reduce (fun n1 n2 -> n1 + n2)
+         
--- CalculatorServiceTests/UnitTest1.fs
+++ CalculatorServiceTests/UnitTest1.fs
@@ -11,17 +11,21 @@ module TestClass  =
     [<TestCase("", ExpectedResult  = 0, Description = "for an empty string it will return 0")>]
     [<TestCase("1", ExpectedResult  = 1, Description = "for a single sumber it will return that number")>]
     [<TestCase("1,2", ExpectedResult  = 3, Description = "for two numbers with comma it will return their sum")>]
-     let AddTwoNumbers_CommaDelimiter_Returns_Sum inputString =
-         Calculate.Add inputString
+        let AddTwoNumbers_CommaDelimiter_Returns_Sum inputString =
+            Calculate.Add inputString
 
 
-   [<TestCase("1,2,3", ExpectedResult  = 6)>]
-   [<TestCase("3,4,5,6", ExpectedResult  = 18)>]
-       let AddMoreThanTwoNumbers_CommaDelimiter_Returns_Sum inputString =
+    [<TestCase("1,2,3", ExpectedResult  = 6)>]
+    [<TestCase("3,4,5,6", ExpectedResult  = 18)>]
+        let AddMoreThanTwoNumbers_CommaDelimiter_Returns_Sum inputString =
            Calculate.Add inputString
 
-   [<TestCase("1\n2,3", ExpectedResult = 6)>]
-    let Accept_Comma_And_newline_as_Seperators inputString = 
-        Calculate.Add inputString
+    [<TestCase("1\n2,3", ExpectedResult = 6)>]
+    [<TestCase("1,2\n3", ExpectedResult = 6)>]
+        let Accept_Comma_And_newline_as_Seperators inputString = 
+            Calculate.Add inputString
         
 
+    //[<TestCase("//;\n1;2", ExpectedResult = 3)>]
+    //    let Support_Different_Delimiters inputString = 
+    //        Calculate.Add inputString
