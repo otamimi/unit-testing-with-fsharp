c806e27317625525551564e768b01a0ec328c236
osama <osama.tamimi@outlook.com>
2019-10-06 19:13:23 +0300

    refactor

--- CalculatorService/Library.fs
+++ CalculatorService/Library.fs
@@ -4,13 +4,18 @@ open System
 
 module Calculate =
 
-   
+    let decompose (str:string) = 
+        match str with
+        | str when str.StartsWith("//") ->
+            let delim = str.[2]
+            ([| delim |], str.[3..])
+        | str -> ([| ',';'\n' |], str)
+
     let Add str =
           match str with
           | "" -> 0
-          | _ when str.StartsWith("//") -> 
-                let delimiter = str.[2]
-                str.[3..].Split(delimiter) |> Array.map int |> Array.sum
-          | _  ->
-              str.Split(',','\n') |> Array.map int |> Array.sum
+          | _ ->
+                let delimiter, numbers = decompose str
+                numbers.Split(delimiter) |> Array.map int |> Array.sum
+         
          
